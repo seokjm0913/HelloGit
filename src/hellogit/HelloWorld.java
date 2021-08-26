735a61b7b2ff8384edcb5b88c750a84e62d3e3f4
SJM <sukjm0915@gmail.com>
2021-08-26 10:13:55 +0900

    test 모듈 완료

--- src/hellogit/HelloWorld.java
+++ src/hellogit/HelloWorld.java
@@ -5,15 +5,13 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("지금 git 수업 중");
-		System.out.println("집중하세요~~ 단 한번만 수업합니다.");
-		
+		System.out.println("집중하세요~~ 단 한번만 수업합니다.");	
 		test();
-	
-	
 	}
 	
 	private static void test() {
 		System.out.println("새모듈 작업 완성~~");
+		System.out.println("머지완료~~~~~");
 	}
 
 }
